ec01998c70aa91c8388f1eec0db1fd489ec4732c
connor-jason <connorejason@gmail.com>
2024-08-31 23:48:09 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import EnterName from './components/EnterName';
+import Events from './components/Events';
+import Summary from './components/Summary';
+import Calendar from './components/Calendar';
 import './App.css';
 
 function App() {
+
+  const [currentStep, setCurrentStep] = useState(1);
+  const [name, setName] = useState('');
+
+  const handleName = (name : string) => {
+    setCurrentStep(2);
+    setName(name)
+  };
+
+  const handleEvent = () => {
+    setCurrentStep(3);
+  };
+
+  const handleData = () => {
+    setCurrentStep(4);
+  };
+
+  const handleSubmit = () => {
+    setCurrentStep(1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex flex-row w-1/2">
+      <div className="">
+          <div>person goes here</div>
+      </div>
+      <div>
+        {currentStep === 1 && (
+          <EnterName handleName={handleName} />
+          )}
+        {currentStep === 2 && (
+          <div>
+            <Events nextStep={handleEvent} />
+            <div>{name}</div>
+          </div>
+          )}
+        {currentStep === 3 && (
+          <Calendar nextStep={handleData} />
+          )}
+        {currentStep === 4 && (
+          <Summary nextStep={handleSubmit} />
+          )}
+      </div>
     </div>
   );
 }
